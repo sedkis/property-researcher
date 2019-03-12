0b63288e3692fa383c0209cf8f125519ee962211
Abou-Shamalah, Sedky <Sedky.Abou-Shamalah@londonlife.com>
2019-03-12 14:22:21 -0400

    Adds the form with real time updating

--- web-ui/src/form/Form.js
+++ web-ui/src/form/Form.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            lengthYears: 5,
+            sprintLength: 12,
+            interest: 0.05
+        }
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleLengthChange = this.handleLengthChange.bind(this);
+        this.handleSprintLengthChange = this.handleSprintLengthChange.bind(this);
+        this.handleLengthChange = this.handleLengthChange.bind(this);
+    }
+
+    handleNameChange(event) {
+        this.setState({name: event.target.value})
+    }
+    handleLengthChange(event) {
+        this.setState({lengthYears: event.target.value})
+    }
+
+    handleSprintLengthChange(event) {
+        this.setState({sprintLength: event.target.value})
+    }
+
+    render() {    
+
         return (
+        <div>
             <form>
-            <label>
-              Principal:
-              <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Submit" />
-          </form>
+
+                <label>
+                    Lab Name:
+                    <input type="text" value={this.state.name} onChange={this.handleNameChange}/>
+                </label>
+
+                <br></br>
+
+                <label>
+                    MVP Release Years:
+                    <input type="number" value={this.state.lengthYears} onChange={this.handleLengthChange}/>
+                </label>
+
+                <br></br>
+
+                <label>
+                    Sprint Length:
+                    <select value={this.state.sprintLength} onChange={this.handleSprintLengthChange}>
+                        <option value={26}>Biweekly</option>
+                        <option value={12}>Monthly</option>
+                    </select>
+                </label>
+
+                <br></br>
+
+                <label>
+                    Interest Rate:
+                    <input type="number" value={this.state.interest} onChange={this.handleLengthChange}/>
+                </label>
+            
+                <br/>
+                <input type="submit" value="Submit" />
+            
+            </form>
+
+            <br/>
+            Return: { this.state.lengthYears * this.state.sprintLength * this.state.interest }
+            <br></br>
+            Email results to me:
+        </div>
         )
     }
 }
