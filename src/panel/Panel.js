af1322087298552be5acad54bdd47c811f42933b
Sedky <sedkyaboushamalah@gmail.com>
2020-01-05 14:26:52 -0500

    refactored the panel a bit

--- src/panel/Panel.js
+++ src/panel/Panel.js
@@ -5,16 +5,57 @@ class Panel extends Component {
     render() {
         return (
             <div className="panel">
-                <h4>your Listings   </h4>
-                <p>Bloop</p>
-                <p>Bloop</p>
-                <p>Bloop</p>
-                <p>Bloop</p>
-                <p>Bloop</p>
-                <p>Bloop</p>
+                <h4>Potential Properties</h4>
+                {this.getPanelContent()}
             </div>
         )
     }
+
+    getPanelContent() {
+        var panelContent;
+        if (mockPayload && mockPayload.length > 0) {
+            panelContent =
+                <ul>
+                    {mockPayload.map((value, index) => <li key={index}>{value.address}</li>)}
+                </ul>
+        } else {
+            panelContent =
+                <p>
+                    Add something new!
+                </p>
+        }
+
+        return panelContent
+    }
 }
+
+
+
+const mockPayload = [
+    {
+        "address": "55 hale street",
+        "insurance": 175,
+        "interest": 3.54,
+        "maintenance": 200,
+        "mortgageLengthYears": 25,
+        "paymentFreqPerYear": 12,
+        "principal": 450000,
+        "propertyTax": 150,
+        "rent": 2500,
+        "utilities": 400
+    },
+    {
+        "address": "810 wild rose",
+        "insurance": 175,
+        "interest": 3.54,
+        "maintenance": 200,
+        "mortgageLengthYears": 25,
+        "paymentFreqPerYear": 12,
+        "principal": 200000,
+        "propertyTax": 150,
+        "rent": 2500,
+        "utilities": 400
+    }
+]
 
 export default Panel;
